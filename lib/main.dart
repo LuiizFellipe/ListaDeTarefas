5729983a01e80ebf7a659e58829ee7672380e161
Luiizfellipe <luiizfellipe12@gmail.com>
2020-04-06 14:03:38 -0300

    Criado a lista

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-
 void main() {
   runApp(MaterialApp(home: Home()));
 }
@@ -15,34 +14,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List _toDoList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Lista de tarefas"), backgroundColor: Colors.blueAccent, centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
-            child: Row(children: <Widget>[
-              Expanded(child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Adicionar tarefa", labelStyle: TextStyle(color: Colors.blueAccent)
-                ),
-              ),),
-              RaisedButton(
-                color: Colors.blueAccent, child: Text("ADD"),textColor: Colors.white,
-                onPressed: (){},
-              )
-            ],),
-          )
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text("Lista de tarefas"),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Adicionar tarefa",
+                          labelStyle: TextStyle(color: Colors.blueAccent)),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text("ADD"),
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10),
+                  itemCount: _toDoList.length,
+                  itemBuilder: (context, index) {
+                    return CheckboxListTile(
+                      title: Text(_toDoList[index]["title"]),
+                      value: _toDoList[index]["ok"],
+                      secondary: CircleAvatar(
+                        child: Icon(
+                            _toDoList[index]["ok"] ? Icons.check : Icons.error),
+                      ), onChanged: (bool value) {},
+                    );
+                  }),
+            )
+          ],
+        ));
   }
 
   Future<File> _getFile() async {
@@ -56,15 +76,12 @@ class _HomeState extends State<Home> {
     return file.writeAsString(data);
   }
 
-  Future<String> _readData() async
-  {
-      try {
-    final file = await _getFile();
-    return file.readAsString();
-  } 
-  catch (e) {
-    return null;
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
-  }
-
 }
