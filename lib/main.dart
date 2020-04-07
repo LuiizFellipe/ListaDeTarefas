12c1caedededa87ce83e89bf7571202dc34b4966
Luiizfellipe <luiizfellipe12@gmail.com>
2020-04-07 16:17:24 -0300

    Projeto concluido.

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,19 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+      setState(() {
+            _toDoList.sort((a,b){
+      if(a["ok"] && !b["ok"]) return 1;
+      else if(b["ok"] && !a["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+    return  null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,10 +86,10 @@ class _HomeState extends State<Home> {
               ),
             ),
             Expanded(
-              child: ListView.builder(
+              child: RefreshIndicator(child: ListView.builder(
                   padding: EdgeInsets.only(top: 10),
                   itemCount: _toDoList.length,
-                  itemBuilder: buildItem),
+                  itemBuilder: buildItem), onRefresh: _refresh),
             )
           ],
         ));
@@ -130,6 +143,7 @@ class _HomeState extends State<Home> {
               ),
               duration: Duration(seconds: 5),);
 
+              Scaffold.of(context).removeCurrentSnackBar();    // ADICIONE ESTE COMANDO
               Scaffold.of(context).showSnackBar(snack);
         });
       },
